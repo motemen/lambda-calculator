f3c2052c25f569cfa1850fdf250ac8112373e5bd
motemen <motemen@gmail.com>
2014-07-26 21:14:43 +0900

    stringify

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -2,31 +2,24 @@ package net.tokyoenvious.lambdacalc
 
 import util.parsing.combinator._
 
-/**
- * Syntax:
- *   term ::= variable
- *          | term term
- *          | '\' variable '.' term
- */
 object Parser extends RegexParsers with PackratParsers {
   lazy val variable: PackratParser[NamedVar] = "[a-zA-Z][a-zA-Z0-9]*".r ^^ {
     case name => NamedVar(name)
   }
 
-  lazy val abstraction: PackratParser[NamedAbs] = "\\" ~> variable ~ "." ~ term ^^ {
+  lazy val abstraction: PackratParser[NamedAbs] = """[\\λ]""".r  ~> variable ~ "." ~ term ^^ {
     case (v ~ _ ~ b) => NamedAbs(v, b)
   }
 
   lazy val application: PackratParser[NamedTerm] = rep1(abstraction | variable | parenTerm) ^^ {
     case (t :: ts) => (t /: ts) { NamedApp }
-    case _ => ??? // Could not occur
   }
 
   lazy val parenTerm: PackratParser[NamedTerm] = "(" ~> term <~ ")"
 
   lazy val term: PackratParser[NamedTerm] =
     abstraction | application | variable | parenTerm |
-      failure("expected '\\', '(' or variable")
+      failure("""expected '\', 'λ', '(' or variable name""")
 
   def parse(input: String): ParseResult[NamedTerm] = parseAll(term, input)
 }
