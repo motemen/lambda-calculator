53b02b898edc96e6b40804b65d359f70cf3b6214
motemen <motemen@gmail.com>
2014-07-28 13:55:53 +0900

    small fix

--- src/main/scala/StringMacro.scala
+++ src/main/scala/StringMacro.scala
@@ -14,17 +14,19 @@ object StringMacro {
     "tru"  -> """\x.\y.x""",
     "fls"  -> """\x.\y.y""",
 
-    "pair" -> """\x.\y.\f.f x y""",
-    "fst"  -> """\p.p (\x.\y.x)""",
-    "snd"  -> """\p.p (\x.\y.y)""",
+    "pair" -> """\f.\s.\b.b f s""",
+    "fst"  -> """\p.p $tru""",
+    "snd"  -> """\p.p $fls""",
 
-    "c0"     -> """\s.\z.z""",
-    "c1"     -> """\s.\z.s z""",
-    "c2"     -> """\s.\z.s (s z)""",
-    "c3"     -> """\s.\z.s (s (s z))""",
-    "plus"   -> """\n.\m.\s.\z.n s (m s z)""",
-    "iszero" -> """\n.n (\x.$fls) $tru""",
+    "c0"    -> """\s.\z.z""",
+    "c1"    -> """\s.\z.s z""",
+    "c2"    -> """\s.\z.s (s z)""",
+    "c3"    -> """\s.\z.s (s (s z))""",
+    "plus"  -> """\m.\n.\s.\z.m s (n s z)""",
+    "iszro" -> """\m.m (\x.$fls) $tru""",
 
-    "Y" -> """\f. (\x.f \y. x x y) (\x.f \y. x x y)"""
+    "fix" -> """\f.(\x.f (\y.x x y)) (\x.f (\y.x x y))""",
+
+    "omega" -> """(\x.x x)(\x.x x)"""
   ))
 }
