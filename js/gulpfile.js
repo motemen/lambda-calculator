738e5a1abce3d8a6b6ca602ff3537779a74d3fe0
motemen <motemen@gmail.com>
2014-07-30 13:49:59 +0900

    gulp

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,22 +1,50 @@
-var gulp       = require('gulp'),
-    $          = require('gulp-load-plugins')(),
-    spawn      = require('child_process').spawn;
+var gulp        = require('gulp'),
+    $           = require('gulp-load-plugins')(),
+    spawn       = require('child_process').spawn,
+    browserSync = require('browser-sync');
 
-gulp.task('production', [ 'sbt:js' ], function () {
-    var js = gulp.src('target/scala-2.11/js-opt.js')
-                 .pipe(gulp.dest('publish/js'));
-
-    gulp.src('src/main/html/call-by-value-untyped.html')
-        .pipe($.inject(js, { addRootSlash: false, ignorePath: 'publish/' }))
-        .pipe(gulp.dest('publish'));
-});
-
-gulp.task('sbt:js', function (done) {
-    var sbt = spawn('sbt', [ 'js/fullOptJS' ], { cwd: __dirname + '/..', stdio: 'inherit' });
+function sbt(args, done) {
+    var sbt = spawn('sbt', args, { cwd: __dirname + '/..', stdio: 'inherit' });
     sbt.on('close', function (exitCode) {
         if (exitCode !== 0) {
             throw "sbt exited with code " + exitCode;
         }
-        done();
+        if (done) done();
     });
+}
+
+var paths = {
+    js: {
+        fast: 'target/scala-2.11/js-fastopt.js',
+        full: 'target/scala-2.11/js-opt.js'
+    },
+    html: 'src/main/html/call-by-value-untyped.html'
+}
+
+function buildHTML(js, dir) {
+    var js = gulp.src(js).pipe(gulp.dest(dir + '/js'));
+
+    return gulp.src(paths.html)
+        .pipe($.inject(js, { addRootSlash: false, ignorePath: dir }))
+        .pipe(gulp.dest(dir));
+}
+
+gulp.task('html', function () {
+    return buildHTML(paths.js.fast, '.tmp');
+});
+
+gulp.task('serve', [ 'html' ], function (done) {
+    browserSync({ server: { baseDir: '.tmp' } });
+
+    sbt([ 'project js', '~fastOptJS' ]);
+    gulp.watch('target/scala-2.11/js-fastopt.js', { debounceDelay: 2000 }, [ 'html' ]);
+    gulp.watch('.tmp/**', browserSync.reload);
+});
+
+gulp.task('stage', [ 'sbt:fullOptJS' ], function () {
+    return buildHTML(paths.js.full, 'publish');
+});
+
+gulp.task('sbt:fullOptJS', function (done) {
+    sbt(['js/fullOptJS'], done);
 });
